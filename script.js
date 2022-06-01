ba13b886d9003433ecb8978bae7fe4b09e049e0b
grahamcottridge <codinggc@gmail.com>
2022-06-01 23:06:26 +0300

    add comment

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Dark Mode Styles
 function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
